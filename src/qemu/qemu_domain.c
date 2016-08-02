98b80b6e7ece5b84e005f57dcc8cfd9cdad99799
Martin Kletzander <mkletzan@redhat.com>
2016-08-02 13:32:34 +0200

    qemu: Remove unnecessary label and its only reference
    https://bugzilla.redhat.com/show_bug.cgi?id=1168453
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>
    (cherry picked from commit 779a4ea9066560b92b0df462b5a6d5c127c15920)
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

--- src/qemu/qemu_domain.c
+++ src/qemu/qemu_domain.c
@@ -4114,7 +4114,6 @@ qemuDomainCheckDiskPresence(virQEMUDriverPtr driver,
                             virDomainObjPtr vm,
                             bool cold_boot)
 {
-    int ret = -1;
     size_t i;
 
     VIR_DEBUG("Checking for disk presence");
@@ -4141,13 +4140,10 @@ qemuDomainCheckDiskPresence(virQEMUDriverPtr driver,
         if (qemuDomainCheckDiskStartupPolicy(driver, vm, idx, cold_boot) >= 0)
             continue;
 
-        goto error;
+        return -1;
     }
 
-    ret = 0;
-
- error:
-    return ret;
+    return 0;
 }
 
 /*
