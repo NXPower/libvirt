dda3a6e43f10a8a01587ae43b8ecc2d84ebfa725
Ján Tomko <jtomko@redhat.com>
2016-08-16 12:49:45 +0200

    conf: report an error message for non-existing USB hubs
    If any of the devices referenced a USB hub that does not exist,
    defining the domain would either fail with:
    error: An error occurred, but the cause is unknown
    (if only the last hub in the path is missing)
    or crash.
    
    Return a proper error instead of crashing.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1367130
    (cherry picked from commit ef66bd5df8af93db5a77f0c1f33182139c9b63b3)
    Signed-off-by: Ján Tomko <jtomko@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -1202,6 +1202,9 @@ mymain(void)
     DO_TEST_PARSE_ERROR("usb-hub-conflict",
             QEMU_CAPS_CHARDEV, QEMU_CAPS_USB_HUB,
             QEMU_CAPS_NODEFCONFIG);
+    DO_TEST_PARSE_ERROR("usb-hub-nonexistent",
+            QEMU_CAPS_CHARDEV, QEMU_CAPS_USB_HUB,
+            QEMU_CAPS_NODEFCONFIG);
     DO_TEST("usb-port-missing",
             QEMU_CAPS_CHARDEV, QEMU_CAPS_USB_HUB,
             QEMU_CAPS_NODEFCONFIG);
