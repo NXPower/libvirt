c634c432192be4d3c2324f2e300d24bf16954323
John Ferlan <jferlan@redhat.com>
2016-11-04 10:29:45 +0100

    conf: Remove incorrect check when encoding shmem audit message
    Remove the !size check since size is initialized to NULL and thus
    causing the condition to always be true
    
    (cherry picked from commit c951cdbff94ff577c5caa3dfff4153a161c073e6)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1389013
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

--- src/conf/domain_audit.c
+++ src/conf/domain_audit.c
@@ -982,7 +982,7 @@ virDomainAuditShmem(virDomainObjPtr vm,
 
     virUUIDFormat(vm->def->uuid, uuidstr);
 
-    if (!vmname || !src || !size || !shmem ||
+    if (!vmname || !src || !shmem ||
         virAsprintfQuiet(&size, "%llu", def->size) < 0) {
         VIR_WARN("OOM while encoding audit message");
         goto cleanup;
@@ -996,7 +996,7 @@ virDomainAuditShmem(virDomainObjPtr vm,
 
     VIR_AUDIT(VIR_AUDIT_RECORD_RESOURCE, success,
               "virt=%s resrc=shmem reason=%s %s uuid=%s size=%s %s %s",
-              virt, reason, vmname, uuidstr, size ?: "?", shmem, src);
+              virt, reason, vmname, uuidstr, size, shmem, src);
 
  cleanup:
     VIR_FREE(vmname);
