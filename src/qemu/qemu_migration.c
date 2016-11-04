d169cfaeb43ae927cb3bc12773fc58685f1a4b74
Martin Kletzander <mkletzan@redhat.com>
2016-11-04 10:29:48 +0100

    qemu: Disable migration with ivshmem
    It was never safe anyway and as such shouldn't have been enabled in the
    first place.  Future patches will allow hot-(un)pluging of some ivshmem
    devices as a workaround.
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>
    (cherry picked from commit d17fab69be7a73336d388b805c282037ffb29647)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1392031
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

--- src/qemu/qemu_migration.c
+++ src/qemu/qemu_migration.c
@@ -2350,6 +2350,12 @@ qemuMigrationIsAllowed(virQEMUDriverPtr driver,
                 return false;
             }
         }
+
+        if (vm->def->nshmems) {
+            virReportError(VIR_ERR_OPERATION_INVALID, "%s",
+                           _("migration with shmem device is not supported"));
+            return false;
+        }
     }
 
     return true;
